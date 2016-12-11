6f336c8280f25530482b654f45323103c0b41d0e
Dissupos <galepwnz@gmail.com>
2016-12-11 19:51:34 +0100

    refactor

--- src/main/java/cz/project/c3/domain/base/BaseObject.java
+++ src/main/java/cz/project/c3/domain/base/BaseObject.java
@@ -6,50 +6,26 @@ import org.springframework.data.annotation.LastModifiedDate;
 import javax.persistence.*;
 import java.util.Date;
 
-/**
- * All persist object must inherit this abstract object
- * 
- * @author dis
- * @since 0.0.1
- */
 @MappedSuperclass
 public abstract class BaseObject {
-    // ============= Constants =================================================
-    // ============= Attributes ================================================
-    /**
-     * Identificator
-     * 
-     * @since 0.0.1
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
-    /**
-     * Date when object was created
-     * 
-     * @since 0.0.1
-     */
     @CreatedDate
     @Column(name = "created_date", nullable = false, updatable = false)
     private Date createdDate = new Date();
 
-    /**
-     * Date when object was changed
-     * 
-     * @since 0.0.1
-     */
     @LastModifiedDate
     @Column(name = "last_modified_date")
     private Date lastModifiedDate = new Date();
 
-    // ============= Constructors ==============================================
     public BaseObject() {
         super();
     }
 
-    // ============= Getter/Setters ============================================
     public Long getId() {
         return id;
     }
@@ -73,7 +49,5 @@ public abstract class BaseObject {
     public void setLastModifiedDate(Date lastModifiedDate) {
         this.lastModifiedDate = lastModifiedDate;
     }
-    // ============= Override, Implements ======================================
-    // ============= Methods ===================================================
 
 }
--- src/main/java/cz/project/c3/domain/person/Address.java
+++ src/main/java/cz/project/c3/domain/person/Address.java
@@ -25,30 +25,18 @@ public class Address extends BaseObject {
         this.city = city;
     }
 
-    /**
-     * @return the country
-     */
     public String getCountry() {
         return country;
     }
 
-    /**
-     * @param country the country to set
-     */
     public void setCountry(String country) {
         this.country = country;
     }
 
-    /**
-     * @return the city
-     */
     public String getCity() {
         return city;
     }
 
-    /**
-     * @param city the city to set
-     */
     public void setCity(String city) {
         this.city = city;
     }
